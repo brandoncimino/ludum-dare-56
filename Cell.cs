f02ece010b1a0a7174048a1e606d3dbb037a150d
nicole <nicole.aretz@t-online.de>
2024-10-05 18:35:15 -0500

    - cell moves downward on its own - cell respawns above top edge of screen after leaving screen at the bottom edge

--- Cell.cs
+++ Cell.cs
@@ -3,6 +3,15 @@ using System;
 
 public partial class Cell : Node3D
 {
+	private Transform3D transform;
+	[Export()] private float speed = 1;
+
+	public Cell()
+	{
+		transform = this.GetTransform();
+		
+	}
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -11,5 +20,39 @@ public partial class Cell : Node3D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		apply_movement((float) delta);
 	}
+
+	private void apply_movement(float delta)
+	{
+		// get current position
+		Vector3 position = this.Position;
+		
+		// move downwards
+		position += Vector3.Down * (speed * delta);
+		
+		// check for domain boundary and adjust
+		position = apply_boundary_conditions(position);
+		
+		// apply changes in the position
+		this.Position = position;
+	}
+
+	private Vector3 apply_boundary_conditions(Vector3 position)
+	{
+		
+		// check if below bottom edge of screen
+		// bottom edge of screen is at y=-50, using an extra 10% buffer
+		if (position.Y < -55)
+		{
+			// place object above the top of the screen
+			position.Y = 55;
+		}
+
+		return (position);
+	}
+
+	
+	
+	
 }
