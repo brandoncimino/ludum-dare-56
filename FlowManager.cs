e9b7a168c15da9d605585a22ad27c18b4a9220b7
nicole <nicole.aretz@t-online.de>
2024-10-06 18:10:13 -0500

    sugar gets eaten

--- FlowManager.cs
+++ FlowManager.cs
@@ -78,7 +78,7 @@ public partial class FlowManager : Node
 	private void spawn_a_sugar()
 	{
 		// Create new instance of cell scene
-		Sugar sugar = SugarScene.Instantiate<Sugar>();
+		FlowParticle sugar = SugarScene.Instantiate<FlowParticle>();
 		
 		// spawn into the world
 		spawn_a_particle(true, sugar);
